f0bd2cbe03c91a76454023003b18f84bb6c5af74
creepid <mikhail.complete@gmail.com>
2018-03-30 18:19:28 +0300

    device type filtering add

--- app/spaces/space-devices/device-list.component.ts
+++ app/spaces/space-devices/device-list.component.ts
@@ -1,10 +1,41 @@
 import { Component, Input } from '@angular/core'
-import { IDevice } from '../index';
+import { IDevice, DeviceType } from '../index';
+import { OnChanges } from '@angular/core';
 
 @Component({
     selector: 'device-list',
-    templateUrl: 'app/spaces/space-devices/device-list.component.html'
+    templateUrl: 'app/spaces/space-devices/device-list.component.html',
+    styles: ['collapsible-well h6 {margin-top:-5px; margin-bottom:10px }']
 })
-export class DeviceListComponent {
-    @Input() devices:IDevice[]
+export class DeviceListComponent implements OnChanges {
+    @Input() devices: IDevice[]
+    @Input() filterBy: string;
+    visibleDevices: IDevice[] = [];
+
+    ngOnChanges(): void {
+        if (this.devices) {
+            this.filterDevices(this.filterBy);
+        }
+    }
+
+    filterDevices(filter) {
+        if (filter === 'all') {
+            this.visibleDevices = this.devices.slice(0);
+        } else {
+            let filteredDeviceTypes: DeviceType[] = [];
+            if (filter === 'sensor') {
+                filteredDeviceTypes.push(DeviceType.Sensor)
+            } else if (filter === 'switch') {
+                filteredDeviceTypes.push(DeviceType.Switch)
+                filteredDeviceTypes.push(DeviceType.AdjustableSwitch)
+            } else if (filter === 'alarm') {
+                filteredDeviceTypes.push(DeviceType.SignalingElement)
+            }
+
+            this.visibleDevices = this.devices.filter(device => {
+                return filteredDeviceTypes.indexOf(device.type) !== -1
+            })
+        }
+    }
+
 }
