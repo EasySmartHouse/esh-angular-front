7a1baee9dbba0998c07d325207cfec36e8e44875
creepid <mikhail.complete@gmail.com>
2018-04-15 16:40:46 +0300

    auth service put url test

--- app/user/auth.service.spec.ts
+++ app/user/auth.service.spec.ts
@@ -1,6 +1,7 @@
 import {} from 'jasmine';
 import { AuthService } from './auth.service';
 import { IUser } from "./user.model";
+import { ESHAppComponent } from '../esh-app.component';
 
 describe('AuthService', () => {
     let authService: AuthService,
@@ -20,6 +21,16 @@ describe('AuthService', () => {
 
             expect(authService.currentUser.firstname).toBe('mike')
             expect(authService.currentUser.lastname).toBe('anotherman')
+        })
+
+        it('should call http.put with the right url', ()=>{
+            let user = {id:15, firstname:'john', lastname:'someone'}
+            authService.currentUser = <IUser>user
+
+            authService.updateCurrentUser('mike', 'anotherman')
+
+            expect(mockHttp.put).toHaveBeenCalledWith(ESHAppComponent.API_URL + '/user/15', 
+            JSON.stringify(user), jasmine.any(Object))
         })
     })
 
