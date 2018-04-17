75b32ff36bad5abf80841fed9face7260240c942
creepid <mikhail.complete@gmail.com>
2018-04-17 12:15:03 +0300

    enable prod mode

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,7 @@
 import { platformBrowserDynamic } from
     '@angular/platform-browser-dynamic'
-
 import { ESHAppModule } from './esh-app.module'
+//import { enableProdMode } from '@angular/core'
 
+//enableProdMode()
 platformBrowserDynamic().bootstrapModule(ESHAppModule)
