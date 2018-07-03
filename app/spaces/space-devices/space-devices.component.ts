4850190a12420d9e4926368fe23bb1c8da36119f
creepid <mikhail.complete@gmail.com>
2018-07-03 18:09:07 +0300

    device value timer update

--- app/spaces/space-devices/space-devices.component.ts
+++ app/spaces/space-devices/space-devices.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core'
 import { SpaceService } from '../shared/space.service';
 import { ActivatedRoute, Params } from '@angular/router';
 import { ISpace, IDevice } from '../index';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
     templateUrl:
@@ -27,6 +28,13 @@ export class SpaceDevicesComponent {
             this.space = data['space']
             this.addMode = false
         })
+
+        let timer = Observable.timer(0, 1000)
+        timer.subscribe(t => 
+            this.spaceService.getSpace(this.space.id).subscribe(
+                space => this.space = space
+            )
+        );
     }
 
     addDevice() {
